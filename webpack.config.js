b6bc30839cfde1f69ef9b8eaa0895ce99e0a1d49
Yutao_Ren <yutaoren118@gmail.com>
2019-03-28 15:19:41 -0700

    add scss loader, override boostrap color

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+        test: /\.scss$/,
+        use: [
+          "style-loader", // 3. creates style nodes from JS strings
+          "css-loader", // 2. translates CSS into CommonJS
+          "sass-loader" // 1. compiles Sass to CSS, using Node Sass by default
+      ]
       },
     ]
   }
