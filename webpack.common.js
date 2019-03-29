979549c5a68a1ae07cb7a9764658e73efc4b5131
Yutao_Ren <yutaoren118@gmail.com>
2019-03-28 23:25:20 -0700

    add html-loader and file-loader, use clean-webpack-plugin to cleanup bundles

--- webpack.common.js
+++ webpack.common.js
@@ -16,8 +16,22 @@ module.exports = {
           "style-loader", // 3. creates style nodes from JS strings
           "css-loader", // 2. translates CSS into CommonJS
           "sass-loader" // 1. compiles Sass to CSS, using Node Sass by default
-      ]
+        ]
       },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath:"imgs"
+          }
+        }
+      }
     ]
   }
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const common = require('./webpack.common');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const merge = require('webpack-merge');
 
 module.exports = merge(common, {
@@ -7,5 +8,5 @@ module.exports = merge(common, {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  },
+  }
 });
