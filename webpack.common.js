baf0c257090294068e348fa308b222f40b5ff9f1
Yutao_Ren <yutaoren118@gmail.com>
2019-03-28 22:53:31 -0700

    split webpack config file into development and production config file separately, use dev-server to load dev-mode app into memory

--- webpack.common.js
+++ webpack.common.js
@@ -3,16 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
-  output: {
-    filename: 'main.[contentHash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html'
     })
-],
+  ],
   module: {
     rules: [
       {
